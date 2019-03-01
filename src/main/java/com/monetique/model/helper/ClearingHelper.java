404811c84a6a17c09fb496fb3f11da56e6db91c8
cherif <moulaye.eli1990@gmail.com>
2019-03-01 08:48:40 +0000

    integration diag classe

--- src/main/java/com/monetique/model/helper/ClearingHelper.java
+++ src/main/java/com/monetique/model/helper/ClearingHelper.java
@@ -16,8 +16,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Stream;
 
-import com.monetique.entities.Clearing;
-import com.monetique.entities.ClearingRejeter;
 
 public class ClearingHelper {
 
@@ -147,159 +145,9 @@ public class ClearingHelper {
 	    return myString.substring(index, Math.min(index + length, myString.length()));
 	}
 	
-	public static ClearingRejeter getClearingRejeterByCl(Clearing i) {
-		ClearingRejeter c= new ClearingRejeter();
-		
-		c.setIndex(i.getIndex());
-		c.setTypeEnregistrement(i.getTypeEnregistrement());
-		c.setNumeroDeSerie(i.getNumeroDeSerie());
-		c.setCodeOperation(i.getCodeOperation());
-		c.setDateDeProcessing(i.getDateDeProcessing());
-		c.setNumeroDeCommercant(i.getNumeroDeCommercant());
-		c.setNumeroDuTerminal(i.getNumeroDuTerminal());
-		c.setCompteCommercant(i.getCompteCommercant());
-		c.setNomCommercant(i.getNomCommercant());
-		c.setLocationCommercant(i.getLocationCommercant());
-		c.setTerritoire(i.getTerritoire());
-		c.setPan(i.getPan());
-		c.setNumeroComptePorteur(i.getNumeroComptePorteur());
-		c.setDateDexpiration(i.getDateDexpiration());
-		c.setFlagDopposition(i.getFlagDopposition());
-		c.setReferenceTransaction(i.getReferenceTransaction());
-		c.setNumeroDeRemise(i.getNumeroDeRemise());
-		c.setDateRemise(i.getDateRemise());
-		c.setNumeroTransaction(i.getNumeroTransaction());
-		c.setDateDeTransaction(i.getDateDeTransaction());
-		c.setNumeroAutorisation(i.getNumeroAutorisation());
-		c.setMontantDautorisation(i.getMontantDautorisation());
-		c.setMontantTransactionGross(i.getMontantTransactionGross());
-		c.setMonnaie(i.getMonnaie());
-		c.setExposantMonnaie(i.getExposantMonnaie());
-		c.setFraisInterchange(i.getFraisInterchange());
-		c.setSigneFraisInterchange(i.getSigneFraisInterchange());
-		c.setCommissionFraisPorteur(i.getCommissionFraisPorteur());
-		c.setMontantNetCreditCommercant(i.getMontantNetCreditCommercant());
-		c.setCommissionCommercantCredit(i.getCommissionCommercantCredit());
-		c.setCodeBanqueEmettrice(i.getCodeBanqueEmettrice());
-		c.setCodeBanqueAcquereur(i.getCodeBanqueAcquereur());
-		c.setFraisConversion(i.getFraisConversion());
-		c.setCodeCategirieCommercant(i.getCodeCategirieCommercant());
-		c.setCodeSysteme(i.getCodeSysteme());
-		c.setFraisCentre(i.getFraisCentre());
-		c.setStatusTransaction(i.getStatusTransaction());
-		c.setNotUsed(i.getNotUsed());
-		c.setReferenceAutorisation(i.getReferenceAutorisation());
-		
-		return c;
-	}
-	
 
 	
-	public static Clearing getClearingItem(String item) throws ParseException {
-		Clearing c= new Clearing();
-		
-		c.setTypeEnregistrement(mySubString(item,1,2));
-		c.setNumeroDeSerie(mySubString(item,3,6));
-		c.setCodeOperation(mySubString(item,9,5));
-		
-		String dateP=mySubString(item,14,6);
-		try {
-			Date date=df.parse(dateP);
-			c.setDateDeProcessing(date);
-		} catch (Exception e) {
-			c.setDateDeProcessing(null);
-		}
-        
-		c.setNumeroDeCommercant(mySubString(item,20,15));
-		c.setNumeroDuTerminal(mySubString(item,35,8));
-		c.setCompteCommercant(mySubString(item,43,24));
-		c.setNomCommercant(mySubString(item,67,25));
-		c.setLocationCommercant(mySubString(item,92,25));
-		c.setTerritoire(mySubString(item,117,1));
-		c.setPan(mySubString(item,118,19));
-		c.setNumeroComptePorteur(mySubString(item,137,24));
-		c.setDateDexpiration(mySubString(item,161,4));
-		c.setFlagDopposition(mySubString(item,165,1));
-		c.setReferenceTransaction(mySubString(item,166,23));
-		c.setNumeroDeRemise(mySubString(item,189,6));
-		c.setDateRemise(mySubString(item,195,6));
-		c.setNumeroTransaction(mySubString(item,201,6));
-		c.setDateDeTransaction(mySubString(item,207,6));
-		c.setNumeroAutorisation(mySubString(item,213,6));
-		c.setMontantDautorisation(mySubString(item,219,15));
-		c.setMontantTransactionGross(mySubString(item,234,15));
-		c.setMonnaie(mySubString(item,249,3));
-		c.setExposantMonnaie(mySubString(item,252,1));
-		c.setFraisInterchange(mySubString(item,253,15));
-		c.setSigneFraisInterchange(mySubString(item,268,1));
-		c.setCommissionFraisPorteur(mySubString(item,269,15));
-		c.setMontantNetCreditCommercant(mySubString(item,284,15));
-		c.setCommissionCommercantCredit(mySubString(item,299,15));
-		c.setCodeBanqueEmettrice(mySubString(item,314,5));
-		c.setCodeBanqueAcquereur(mySubString(item,319,5));
-		c.setFraisConversion(mySubString(item,324,15));
-		c.setCodeCategirieCommercant(mySubString(item,339,4));
-		c.setCodeSysteme(mySubString(item,343,1));
-		c.setFraisCentre(mySubString(item,344,15));
-		c.setStatusTransaction(mySubString(item,359,1));
-		c.setNotUsed(mySubString(item,360,41));
-		c.setReferenceAutorisation(mySubString(item,401,12));
-		
-//	Reference ref = new Reference(c.getReferenceTransaction(), c.getCodeOperation());
-		
-		
 
-
-	//	CompositeKey key=new CompositeKey(c.getReferenceTransaction(), c.getNumeroAutorisation());
-
-	//	c.setKey(key);
-		return c;
-	}
-	
-	public static Clearing getClearingByClRejeter(ClearingRejeter i) {
-		Clearing c= new Clearing();
-		
-		c.setTypeEnregistrement(i.getTypeEnregistrement());
-		c.setNumeroDeSerie(i.getNumeroDeSerie());
-		c.setCodeOperation(i.getCodeOperation());
-		c.setDateDeProcessing(i.getDateDeProcessing());
-		c.setNumeroDeCommercant(i.getNumeroDeCommercant());
-		c.setNumeroDuTerminal(i.getNumeroDuTerminal());
-		c.setCompteCommercant(i.getCompteCommercant());
-		c.setNomCommercant(i.getNomCommercant());
-		c.setLocationCommercant(i.getLocationCommercant());
-		c.setTerritoire(i.getTerritoire());
-		c.setPan(i.getPan());
-		c.setNumeroComptePorteur(i.getNumeroComptePorteur());
-		c.setDateDexpiration(i.getDateDexpiration());
-		c.setFlagDopposition(i.getFlagDopposition());
-		c.setReferenceTransaction(i.getReferenceTransaction());
-		c.setNumeroDeRemise(i.getNumeroDeRemise());
-		c.setDateRemise(i.getDateRemise());
-		c.setNumeroTransaction(i.getNumeroTransaction());
-		c.setDateDeTransaction(i.getDateDeTransaction());
-		c.setNumeroAutorisation(i.getNumeroAutorisation());
-		c.setMontantDautorisation(i.getMontantDautorisation());
-		c.setMontantTransactionGross(i.getMontantTransactionGross());
-		c.setMonnaie(i.getMonnaie());
-		c.setExposantMonnaie(i.getExposantMonnaie());
-		c.setFraisInterchange(i.getFraisInterchange());
-		c.setSigneFraisInterchange(i.getSigneFraisInterchange());
-		c.setCommissionFraisPorteur(i.getCommissionFraisPorteur());
-		c.setMontantNetCreditCommercant(i.getMontantNetCreditCommercant());
-		c.setCommissionCommercantCredit(i.getCommissionCommercantCredit());
-		c.setCodeBanqueEmettrice(i.getCodeBanqueEmettrice());
-		c.setCodeBanqueAcquereur(i.getCodeBanqueAcquereur());
-		c.setFraisConversion(i.getFraisConversion());
-		c.setCodeCategirieCommercant(i.getCodeCategirieCommercant());
-		c.setCodeSysteme(i.getCodeSysteme());
-		c.setFraisCentre(i.getFraisCentre());
-		c.setStatusTransaction(i.getStatusTransaction());
-		c.setNotUsed(i.getNotUsed());
-		c.setReferenceAutorisation(i.getReferenceAutorisation());
-		
-		return c;
-	}
 	
 	public static void closingStram() throws IOException{
 	 
