679e6cedcccfab953443b980a708790b5b699254
cherif <moulaye.eli1990@gmail.com>
2020-11-26 17:17:41 +0000

    commit

--- src/main/java/com/monetique/security/securityDispatcher/SecurityConstants.java
+++ src/main/java/com/monetique/security/securityDispatcher/SecurityConstants.java
@@ -2,7 +2,7 @@ package com.monetique.security.securityDispatcher;
 
 public class SecurityConstants {
 	
-	public static final String SECRET="moulaye.eli1990@gmail.com";
+	public static final String SECRET="s.moulaye@bpm.mr";
 	//public static final long EXPIRATION_TIME=864_000_000; //10jours
     public static final long EXPIRATION_TIME=1800_000;  //30 minutes
 	//public static final long EXPIRATION_TIME=1_000;  //10 minutes
