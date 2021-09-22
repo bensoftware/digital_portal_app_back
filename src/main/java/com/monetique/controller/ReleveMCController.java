b1063101827d3b6be515970bf205857d52937207
Mohamed Abdel Ghader <bpm0482@bpm.adds>
2021-09-22 11:35:48 +0000

    valid

--- src/main/java/com/monetique/controller/ReleveMCController.java
+++ src/main/java/com/monetique/controller/ReleveMCController.java
@@ -1,17 +1,13 @@
 package com.monetique.controller;
 
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.monetique.dto.Carte;
 import com.monetique.security.securityDispatcher.SecurityConstants;
 import com.monetique.service.ReleveMCService;
@@ -24,19 +20,19 @@ public class ReleveMCController {
 	@Autowired
 	HttpServletResponse httpServletResponse;
 	
-	@PreAuthorize ("hasAnyAuthority ('apprlb', 'validlb')")
+	@PreAuthorize ("hasAnyAuthority ('constsoldemcprep', 'genrelevmcprep')")
 	@RequestMapping(value="/getInfo/{pan}",method=RequestMethod.GET)
 	public @ResponseBody ResponseDto getInfo(@PathVariable String pan) throws Exception {
 		Carte dto=releveMCService.getInfo(pan);
 		return   new ResponseDto(httpServletResponse.getHeader(SecurityConstants.HEADER_STRING), dto);
 	}
-	@PreAuthorize ("hasAnyAuthority ('apprlb', 'validlb')")
+	@PreAuthorize ("hasAnyAuthority ('constsoldemcprep', 'genrelevmcprep')")
 	@RequestMapping(value="/generateRapport/{pan}",method=RequestMethod.GET)
 	public @ResponseBody void generateInfoReleve(@PathVariable String pan) throws Exception {
 		
 		releveMCService.generateInfoReleve(pan,httpServletResponse);
 	}
-	@PreAuthorize ("hasAnyAuthority ('apprlb', 'validlb')")
+	@PreAuthorize ("hasAnyAuthority ('constsoldemcprep', 'genrelevmcprep')")
 	@RequestMapping(value="/generateRapport/{pan}/{dateDu}/{dateAu}",method=RequestMethod.GET)
 	public @ResponseBody void generateInfoBetwwenDateReleve(@PathVariable String pan,@PathVariable long dateDu,@PathVariable long dateAu) throws Exception {
 		releveMCService.generateInfoBetwwenDateReleve(pan,dateDu,dateAu,httpServletResponse);
